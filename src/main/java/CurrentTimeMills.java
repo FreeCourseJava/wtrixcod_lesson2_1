d53a8ca781fad97842e94658288a1e182db1c984
wtrixC0D <wtrixcod@list.ru>
2022-06-21 21:53:04 +0300

    init commit

---
+++ src/main/java/CurrentTimeMills.java
@@ -0,0 +1,18 @@
+public class CurrentTimeMills {
+    public static void main(String[] args) {
+        long currentTimeMillis = System.currentTimeMillis();
+        long currentTime = returnCurrent(currentTimeMillis);
+        System.out.println("Current time is millies - " + currentTime);
+    }
+
+    public static long returnCurrent(long currentTimeMillis) {
+        long xtype = 10000;
+        if ((currentTimeMillis + xtype) > currentTimeMillis) {
+            return (currentTimeMillis + xtype);
+        }
+        else {
+        return currentTimeMillis;}
+    }
+
+
+}
